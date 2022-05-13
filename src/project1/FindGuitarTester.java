bfe249af0c521e5c810fd1d38e39efaf522dab02
rs0122 <rs.0122.ssthe2@hotmail.co.jp>
2022-05-13 10:35:04 +0900

    GuitarSpecオブジェクトを作製

--- src/project1/FindGuitarTester.java
+++ src/project1/FindGuitarTester.java
@@ -10,19 +10,20 @@ public class FindGuitarTester {
 	    Inventory inventory = new Inventory();
 	    initializeInventory(inventory);
 
-	    Guitar whatErinLikes = new Guitar("", 0, Builder.FENDER, "Stratocastor", 
-	                                      Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
+	    GuitarSpec whatErinLikes = new GuitarSpec(Builder.FENDER, "Stratocastor",Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
 	    List matchingGuitars = inventory.search(whatErinLikes);
 	    if (!matchingGuitars.isEmpty()) {
 	      System.out.println("Erin, you might like these guitars:");
 	      for (Iterator i = matchingGuitars.iterator(); i.hasNext(); ) {
 	        Guitar guitar = (Guitar)i.next();
+	        GuitarSpec spec = guitar.getSpec();
 	        System.out.println("  We have a " +
-	          guitar.getBuilder() + " " + guitar.getModel() + " " +
-	          guitar.getType() + " guitar:\n     " +
-	          guitar.getBackWood() + " back and sides,\n     " +
-	          guitar.getTopWood() + " top.\n  You can have it for only $" +
-	          guitar.getPrice() + "!\n  ----");
+	        	spec.getBuilder() + " " + spec.getModel() + " " +
+	        	spec.getType() + " guitar:\n     " +
+	        	spec.getBackWood() + " back and sides,\n     " +
+	        	spec.getTopWood() + " top.\n  You can have it for only $" +
+	        	guitar.getPrice() + "!\n  ----"
+	        );
 	      }
 	    } else {
 	      System.out.println("Sorry, Erin, we have nothing for you.");
--- src/project1/Guitar.java
+++ src/project1/Guitar.java
@@ -4,20 +4,14 @@ public class Guitar {
 
 	  private String serialNumber, model;
 	  private double price;
-	  private Builder builder;
-	  private Type type;
-	  private Wood backWood, topWood;
+	  GuitarSpec spec;
 
 	  public Guitar(String serialNumber, double price,
 	                Builder builder, String model, Type type,
 	                Wood backWood, Wood topWood) {
 	    this.serialNumber = serialNumber;
 	    this.price = price;
-	    this.builder = builder;
-	    this.model = model;
-	    this.type = type;
-	    this.backWood = backWood;
-	    this.topWood = topWood;
+	    this.spec = new GuitarSpec(builder, model, type, backWood, topWood);
 	  }
 
 	  public String getSerialNumber() {
@@ -30,20 +24,8 @@ public class Guitar {
 	  public void setPrice(float newPrice) {
 	    this.price = newPrice;
 	  }
-	  public Builder getBuilder() {
-	    return builder;
-	  }
-	  public String getModel() {
-	    return model;
-	  }
-	  public Type getType() {
-	    return type;
-	  }
-	  public Wood getBackWood() {
-	    return backWood;
-	  }
-	  public Wood getTopWood() {
-	    return topWood;
+	  public GuitarSpec getSpec() {
+		  return spec;
 	  }
 	}
 
