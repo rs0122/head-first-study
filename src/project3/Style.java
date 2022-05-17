7bc795bb6306d6b55290f899c9d22b00ad500b42
r.saito <r.saito@raxus-create.co.jp>
2022-05-17 16:10:06 +0900

    マンダリンも検索可能に

---
+++ src/project3/Style.java
@@ -0,0 +1,16 @@
+package project3;
+
+public enum Style {
+	A, F;
+
+	public String toString() {
+		switch (this) {
+		case A:
+			return "a";
+		case F:
+			return "f";
+		default:
+			return "unspecified";
+		}
+	}
+}
